80721b28a76d57d1d7a77c2e25f21d484b7f3b86
awais37402 <awaistahir01234@gmail.com>
2025-03-12 11:41:45 +0500

    Update SplashScreen.js

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -2,22 +2,6 @@ import React, { useEffect, useState } from "react";
 import "./SplashScreen.css";
 
 const SplashScreen = ({ onComplete }) => {
-  const message = "Welcome to My Portfolio";
-  const [text, setText] = useState("");
-  const [showCursor, setShowCursor] = useState(true);
-
-  useEffect(() => {
-    let i = 0;
-    const typingSpeed = 80; // Speed of typing effect
-    const displayTime = 3000; // Display time in ms
-
-    const interval = setInterval(() => {
-      setText(message.slice(0, i + 1));
-      i++;
-import React, { useEffect, useState } from "react";
-import "./SplashScreen.css";
-
-const SplashScreen = ({ onComplete }) => {
     const message = "Welcome to My Portfolio";
     const [text, setText] = useState("");
     const [showCursor, setShowCursor] = useState(true);
@@ -56,34 +40,6 @@ const SplashScreen = ({ onComplete }) => {
             </div>
         </section>
     );
-};
-
-export default SplashScreen;
-
-      if (i === message.length) {
-        clearInterval(interval);
-        setTimeout(() => setShowCursor(false), 300);
-      }
-    }, typingSpeed);
-
-    const timeout = setTimeout(() => {
-      onComplete();
-    }, displayTime);
-
-    return () => {
-      clearInterval(interval);
-      clearTimeout(timeout);
-    };
-  }, []);
-
-  return (
-    <div className="splash-screen">
-      <h1>
-        {text}
-        {showCursor && <span className="cursor">|</span>}
-      </h1>
-    </div>
-  );
 };
 
 export default SplashScreen;
