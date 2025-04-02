a243325c13b4e0c0fd7e122335130829a896d031
awais37402 <awaistahir01234@gmail.com>
2025-04-03 00:50:11 +0500

    Add files via upload

--- src/components/Graphics.js
+++ src/components/Graphics.js
@@ -5,7 +5,7 @@ import "swiper/css/pagination";
 import { Pagination, Autoplay } from "swiper/modules";
 import "./Graphics.css";
 
-// Import images properly
+// Import images
 import img1 from "../assets/1.png";
 import img2 from "../assets/2.png";
 import img3 from "../assets/3.jpg";
@@ -23,14 +23,14 @@ const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
 const Graphics = () => {
   return (
     <div className="graphics-wrapper">
-      <h2 className="graphics-heading">Graphic Design Projects</h2> {/* Heading is now outside */}
+      <h2 className="graphics-heading">Graphic Design Projects</h2>
       <div className="graphics-container">
         <Swiper
           modules={[Pagination, Autoplay]}
           autoplay={{ delay: 2500 }}
           loop
           pagination={{ clickable: true }}
-          className="graphics-slider"  // ✅ Updated class name
+          className="graphics-slider"
         >
           {images.map((img, index) => (
             <SwiperSlide key={index}>
@@ -38,6 +38,16 @@ const Graphics = () => {
             </SwiperSlide>
           ))}
         </Swiper>
+
+        {/* Behance Link */}
+        <a 
+          href="https://www.behance.net/awaistahir3808" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className="behance-link"
+        >
+          View More on Behance
+        </a>
       </div>
     </div>
   );
